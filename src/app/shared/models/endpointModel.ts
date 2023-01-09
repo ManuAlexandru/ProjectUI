61eff23216e45a60d983f2d9027665a58832b5d8
ManuAlexandru <92298889+ManuAlexandru@users.noreply.github.com>
2023-01-09 10:47:32 +0200

    Refactored endpointModel

--- src/app/shared/models/endpointModel.ts
+++ src/app/shared/models/endpointModel.ts
@@ -4,13 +4,14 @@ import { Injectable } from "@angular/core";
     providedIn: 'root',
   })
 export class EndpointModel{
-public  registerRoute="http://localhost:8080/api/Home/Auth/Register";
-public  loginRoute="http://localhost:8080/api/Home/Auth/Login";
-public addProductRoute="http://localhost:8080/api/Product/CreateProduct";
-public getAllProducts="http://localhost:8080/api/Home/GetAllProducts";
-public getYourProducts="http://localhost:8080/api/Product/GetYourProducts";
-public deleteProduct="http://localhost:8080/api/Product/DeleteProduct";
-public getProduct="http://localhost:8080/api/Product/GetProduct"
-public updateProduct="http://localhost:8080/api/Product/UpdateProduct";
-public updateProductPrice="http://localhost:8080/api/Product/UpdateProductPrice";
+  public port="8081"
+public  registerRoute="http://localhost:"+this.port+"/api/Home/Auth/Register";
+public  loginRoute="http://localhost:"+this.port+"/api/Home/Auth/Login";
+public addProductRoute="http://localhost:"+this.port+"/api/Product/CreateProduct";
+public getAllProducts="http://localhost:"+this.port+"/api/Home/GetAllProducts";
+public getYourProducts="http://localhost:"+this.port+"/api/Product/GetYourProducts";
+public deleteProduct="http://localhost:"+this.port+"/api/Product/DeleteProduct";
+public getProduct="http://localhost:"+this.port+"/api/Product/GetProduct";
+public updateProduct="http://localhost:"+this.port+"/api/Product/UpdateProduct";
+public updateProductPrice="http://localhost:"+this.port+"/api/Product/UpdateProductPrice";
 }
